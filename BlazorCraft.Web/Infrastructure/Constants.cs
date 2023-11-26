462cc21a85ebc3ab88d90c1a8c970a960d0146cd
Peter Noll <noll.peter@hotmail.com>
2023-11-26 15:35:28 +0100

    Update AboutBlazorCraft page with detailed instructions
    The AboutBlazorCraft page has been significantly updated with detailed information about navigating and using the application, licensing, and running tests. In addition, constants for application versions have been added in the Constants.cs file and styling properties for images and dividers have been introduced in a new CSS file.

--- BlazorCraft.Web/Infrastructure/Constants.cs
+++ BlazorCraft.Web/Infrastructure/Constants.cs
@@ -3,5 +3,7 @@
 public class Constants
 {
     // TODO 2 branches change this
-    public const string VERSION = "WithTests";
+    public const string VERSION = WITH_TESTS_VERSION;
+    public const string WITH_TESTS_VERSION = "With tests";
+    public const string WITHOUT_TESTS_VERSION = "Without tests";
 }
