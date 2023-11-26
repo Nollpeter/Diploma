63a0598701f0c099fcdbbba6bfa15b474f7fe8c1
Peter Noll <noll.peter@hotmail.com>
2023-11-26 15:14:26 +0100

    Add introduction and navigation for BlazorCraft Web
    This commit introduces several new features to the BlazorCraft application. It includes the addition of an introduction page, a new navigation menu, and several components. Also, an error boundary and loading progress indicator have been added to improve user experience. Lastly, it adjusts project settings, updates the .gitignore file, and adds some test functionalities.

---
+++ BlazorCraft.Web/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorCraft.Web;
+using BlazorCraft.Web.Tests.Introduction;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddTransient<Test1>();
+
+await builder.Build().RunAsync();
---
+++ BlazorCraft.Web/Tests/Introduction/Test1.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BlazorCraft.Web.Tests.Introduction;
+
+public class Test1 : TestContext
+{
+    public bool RunTest()
+    {
+        string componentName = "HelloWorld";
+        // Megkeresi a komponens típust a név alapján.
+        
+        Type componentType = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(p => p.Name == componentName);
+        if (componentType == null)
+        {
+            throw new ArgumentException($"A {componentName} komponens nem található.", nameof(componentName));
+        }
+
+        return true;
+    }
+
+    public bool RunTest2()
+    {
+        string componentName = "HelloWorld";
+        // Megkeresi a komponens típust a név alapján.
+        Type componentType = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(p => p.Name == componentName);
+        if (componentType == null)
+        {
+            throw new ArgumentException($"A {componentName} komponens nem található.", nameof(componentName));
+        }
+
+        // Meghívja a generikus RenderComponent függvényt a megfelelő típussal.
+        MethodInfo method = GetType().GetMethods().FirstOrDefault(p => p.Name == nameof(RenderComponent) && p.ContainsGenericParameters && p.GetParameters().Length == 1 && p.GetParameters().First().ParameterType == typeof(ComponentParameter[]));
+        MethodInfo genericMethod = method.MakeGenericMethod(componentType);
+        IRenderedComponent<IComponent> invoke =
+            (IRenderedComponent<IComponent>)genericMethod.Invoke(this, new object[0]);
+        
+        invoke.MarkupMatches("<h6>Hello World!</h6>");
+
+        return true;
+    }
+}
